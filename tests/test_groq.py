033f65f826e2af92c1b03709e233ef00f15fe732
Ishwarya <ishwaryavaradharajan18@gmail.com>
2026-07-03 11:09:03 +0530

    Fix deprecated Groq model

--- tests/test_groq.py
+++ tests/test_groq.py
@@ -6,7 +6,7 @@ load_dotenv()
 client = Groq(api_key=os.getenv('GROQ_API_KEY'))
 
 response = client.chat.completions.create(
-    model='llama-3.3-70b-versatile',
+    model='llama-3.1-8b-instant',
     temperature=0.2,
     messages=[{'role': 'user', 'content': 'Say hello in one sentence'}]
 )
